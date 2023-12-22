71f7ef670c2f24beda91d96a4eecf40abb5fb015
Raghad Sad Aldeen <151654896+Raghad-sad-Aldeen@users.noreply.github.com>
2023-12-22 20:05:41 +0200

    adding 3 file , ShareFeed.js and index.html and App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ShareFeed from './ShareFeed';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import PostCard from './PostCard';
+function App() {
+  return (
+    <div className="App">
+      <ShareFeed />
+     {/* <PostCard/> */}
+      {/* Other components and content */}
+    </div>
+  );
+}
+
+export default App;
