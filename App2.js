34ef03277fc948e000e79be2cf129defe374c63d
Raghad Sad Aldeen <151654896+Raghad-sad-Aldeen@users.noreply.github.com>
2023-12-22 21:15:36 +0200

    adding file , App2.js

---
+++ App2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FeedCard from './FeedCard';
+
+// import PostCard from './PostCard';
+function App() {
+  return (
+    <div className="App">
+    
+      <FeedCard/>
+   
+    </div>
+  );
+}
+
+export default App;
