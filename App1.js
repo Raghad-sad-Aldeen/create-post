9d27519a08b1cd740b76ea0c37cf7c317d6204cd
Raghad Sad Aldeen <151654896+Raghad-sad-Aldeen@users.noreply.github.com>
2023-12-22 21:04:55 +0200

    adding 3 file ,PostCard2.js and App1.js and app2.js

---
+++ App1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import PostCard from './PostCard';
+
+// import PostCard from './PostCard';
+function App() {
+  return (
+    <div className="App">
+    
+      <PostCard/>
+   
+    </div>
+  );
+}
+
+export default App;
