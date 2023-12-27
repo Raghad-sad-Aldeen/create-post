ba2ad6e2301819dce3c5e7d573c587c046ebbe8f
Raghad Sad Aldeen <151654896+Raghad-sad-Aldeen@users.noreply.github.com>
2023-12-27 23:18:52 +0200

    It's afinall sest

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+// import './App.css';
+
+import Createpost from "./ComponentCreate/CreatePost";
+
+function App() {
+  return (
+    <div className="App">
+      <Createpost/>
+    </div>
+  );
+}
+
+export default App;
